bdd7ed9ee123200f7b99834ea5cd38235bdfa64a
VloneMe <francisntima@yahoo.com>
2023-08-29 12:44:00 -0700

    Size of Binary tree

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,11 +8,16 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
+	size_t size = 0;
 
-	size_t left_size = binary_tree_size(tree->left);
-	size_t right_size = binary_tree_size(tree->right);
+	if (tree)
+	{
+		size += 1;
 
-	return (left_size + right_size + 1);
+		size += binary_tree_size(tree->left);
+		size += binary_tree_size(tree->right);
+
+	}
+
+	return (size);
 }

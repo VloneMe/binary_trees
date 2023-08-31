8ab566645d5245da3bac11e3eaded5a889dca423
VloneMe <francisntima@yahoo.com>
2023-08-31 01:35:04 -0700

    checks if a binary tree is a valid AVL Tree

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -2,60 +2,73 @@
 #include <limits.h>
 
 /**
- * binary_tree_height - This calculate the height of a binary tree.
+ * get_tree_height - This calculate the height of a binary tree.
  *
  * @tree: Pointer to the root node of the tree.
  * Return: Height of the tree.
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+int get_tree_height(const binary_tree_t *tree)
 {
+	int left_height, right_height;
+
 	if (tree == NULL)
 		return (0);
 
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
+	left_height = get_tree_height(tree->left);
+	right_height = get_tree_height(tree->right);
 
-	return (1 + (left_height > right_height ? left_height : right_height));
+	if (left_height > right_height)
+		return (left_height + 1);
+	else
+		return (right_height + 1);
 }
 
 /**
- * binary_tree_balance - This calculate the balance factor of a binary tree.
+ * calc_balance_factor - This calculate the balance factor of a binary tree.
  *
  * @tree: Pointer to the root node of the tree.
  * Return: Balance factor of the tree.
  */
-int binary_tree_balance(const binary_tree_t *tree)
+int calc_balance_factor(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
 
-	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	return (get_tree_height(tree->left) - get_tree_height(tree->right));
 }
 
 /**
- * binary_tree_is_avl_recursive - This recursive helper function to check if
- * a binary tree is a valid AVL Tree.
+ * validate_avl_recursive - This recursive helper function
+ * to validate if a binary
  *
+ * tree is a proper AVL Tree.
  * @tree: Pointer to the root node of the tree.
- * @min: Minimum value allowed in the tree.
- * @max: Maximum value allowed in the tree.
+ * @min: Minimum allowed value in the tree.
+ * @max: Maximum allowed value in the tree.
  * Return: 1 if tree is a valid AVL Tree, 0 otherwise.
  */
-int binary_tree_is_avl_recursive(const binary_tree_t *tree, int min, int max)
+int validate_avl_recursive(const binary_tree_t *tree, int min, int max)
 {
+	int balance_left, balance_right, balance;
+
 	if (tree == NULL)
 		return (1);
 
-	int balance = binary_tree_balance(tree);
+	if (tree->n > max || tree->n < min)
+		return (0);
+
+	balance = calc_balance_factor(tree);
 
 	if (balance < -1 || balance > 1)
 		return (0);
 
-	if (tree->n < min || tree->n > max)
-		return (0);
+	balance_left = validate_avl_recursive(tree->left, min, tree->n - 1);
+	balance_right = validate_avl_recursive(tree->right, tree->n + 1, max);
 
-	return (binary_tree_is_avl_recursive(tree->left, min, tree->n - 1) &&
-			binary_tree_is_avl_recursive(tree->right, tree->n + 1, max));
+	if (balance_left == 1 && balance_right == 1)
+		return (1);
+
+	return (0);
 }
 
 /**
@@ -69,5 +82,5 @@ int binary_tree_is_avl(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	return (binary_tree_is_avl_recursive(tree, INT_MIN, INT_MAX));
+	return (validate_avl_recursive(tree, INT_MIN, INT_MAX));
 }
